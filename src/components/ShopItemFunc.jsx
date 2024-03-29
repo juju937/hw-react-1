012eac40e9eda53e609a87f49b0098bc6f233064
Elizaveta Filimonova <ellie.filimonova@gmail.com>
2024-03-30 02:08:49 +0300

    Initial commit

---
+++ src/components/ShopItemFunc.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+export const ShopItemFunc = (props) => {
+  const { item } = props;
+
+  return (
+    <div className="main-content">
+      <h2>{item.brand}</h2>
+      <h1>{item.title}</h1>
+      <h3>{item.description}</h3>
+      <div className="decription">
+        {item.descriptionFull}
+      </div>
+      <div className="highlight-window mobile"><div className="highlight-overlay"></div></div>
+      <div className="divider"></div>
+      <div className="purchase-info">
+      <div className="price">{item.currency}{item.price.toLocaleString("en-US", { maximumFractionDigits: 2, minimumFractionDigits: 2 })}</div>
+      <button>Добавить в корзину</button>
+      </div>
+    </div>
+  );
+};
